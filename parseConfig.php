398754464f4b06cc5c9e79219faa34e59342ab09
KO4UYJ <99303085+firealarmss@users.noreply.github.com>
2022-09-18 23:32:17 -0500

    First

---
+++ parseConfig.php
@@ -0,0 +1,30 @@
+<?php
+    $yaml = file_get_contents('/home/uyj/dvmhost/ctrsCC.yml');
+    $parsed = yaml_parse($yaml);
+    $callsign = $parsed['system']['cwId']['callsign'];
+    $identity = $parsed['system']['identity'];
+    $id = $parsed['network']['id'];
+    //var_dump($parsed['protocols']['dmr']['enable']);
+    $dmrEnB = $parsed['protocols']['dmr']['enable'];
+    $dmrEn = $dmrEnB ? 'true':'false';
+    $dmrEn = str_replace(['true', 'false'], ['yes', 'no'], $dmrEn);
+    $p25EnB = $parsed['protocols']['p25']['enable'];
+    $p25En = $p25EnB ? 'true':'false';
+    $p25En = str_replace(['true', 'false'], ['yes', 'no'], $p25En);
+    //$dmrEn = str_replace('false', 'Yes', $dmrEn);
+    //var_dump($parsed);
+    echo "<div style='width: 350px; border:2px solid black; border-style: dashed;'><span style='font-size: 30px;'>";
+    echo "Callsign: " . $callsign;
+    echo "<br />" . "Identity: " . $identity;
+    echo "<br />" . "Id: " . $id;
+    echo "<br />" . "P25 Enabled: "; echo $p25En;
+    echo "<br />" . "DMR Enabled: "; echo $dmrEn;
+    //echo $dmrEn ? 'true' : 'true';
+    echo "</span></div>";
+    ?>
+<html lang="en">
+
+
+
+</html>
+
